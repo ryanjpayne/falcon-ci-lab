b2f44f853bdb6f1cb6b078c2e5e026519bcf1456
Ryan Payne <ryanj.payne@outlook.com>
2022-08-11 20:17:27 -0500

    add us-east-1 handling to deploy and fix entry.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,16 +5,27 @@ NC="\033[0;0m"
 LB="\033[1;34m"
 
 echo -e "$LB\n"
-read -p "Your external IP Address (for remote SSH connection, CIDR format eg. 1.1.1.1/32): " TRUSTED_IP
+read -p "Your external IP Address (for remote SSH connection, CIDR format eg. 1.1.1.1/32): " trustedIp
 echo -e "Initializing environment templates$NC"
-BUCKET_NAME="$(cat /tmp/environment.txt | cut -c -8 | tr _ - | tr '[:upper:]' '[:lower:]')-templatebucket"
-EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
-REGION_CODE="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+bucketName="$(cat /tmp/environment.txt | cut -c -8 | tr _ - | tr '[:upper:]' '[:lower:]')-templatebucket"
+az=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+region="`echo \"$az\" | sed 's/[a-z]$//'`"
 cd /home/ec2-user
-aws s3api create-bucket --bucket $BUCKET_NAME --region $REGION_CODE --create-bucket-configuration LocationConstraint=$REGION_CODE
-aws s3 cp templates/ s3://$BUCKET_NAME --recursive
+if [[ $region = "us-east-1" ]]
+then
+aws s3api create-bucket --bucket $bucketName --region $region
 echo -e "$LB\n"
 echo -e "Standing up environment$NC"
-aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$BUCKET_NAME.s3-$REGION_CODE.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$BUCKET_NAME ParameterKey=RemoteIp,ParameterValue=$TRUSTED_IP --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $REGION_CODE
+aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$bucketName.s3.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$bucketName ParameterKey=RemoteIp,ParameterValue=$trustedIp --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $region
+else
+aws s3api create-bucket --bucket $bucketName --region $region --create-bucket-configuration LocationConstraint=$region
+echo -e "$LB\n"
+echo -e "Standing up environment$NC"
+aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$bucketName.s3-$region.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$bucketName ParameterKey=RemoteIp,ParameterValue=$trustedIp --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $region
+fi
+aws s3 cp templates/ s3://$bucketName --recursive
+echo -e "$LB\n"
+echo -e "Standing up environment$NC"
+aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$bucketName.s3-$region.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$bucketName ParameterKey=RemoteIp,ParameterValue=$trustedIp --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $region
 echo -e "The Cloudformation stack will take 5-10 minutes to complete$NC"
-echo -e "\n\nCheck the status at any time with the command \n\naws cloudformation describe-stacks --stack-name falcon-ci-lab-stack --region $REGION_CODE$NC\n\n"
+echo -e "\n\nCheck the status at any time with the command \n\naws cloudformation describe-stacks --stack-name falcon-ci-lab-stack --region $region$NC\n\n"
