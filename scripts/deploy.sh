9cfffc34026bd08837a79c47c6dccae65067c302
Ryan Payne <ryanj.payne@outlook.com>
2022-09-01 12:46:04 -0500

    remove remote ip and add hpi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ NC="\033[0;0m"
 LB="\033[1;34m"
 
 echo -e "$LB\n"
-read -p "Please enter your external IP Address (for remote SSH connection, CIDR format eg. 1.1.1.1/32): " trustedIp
 echo -e "Initializing environment templates$NC"
 bucketName="$(cat /tmp/environment.txt | cut -c -8 | tr _ - | tr '[:upper:]' '[:lower:]')-templatebucket"
 az=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
@@ -28,13 +27,13 @@ aws s3api create-bucket --bucket $bucketName --region $region
 aws s3 cp templates/ s3://$bucketName --recursive
 echo -e "$LB\n"
 echo -e "Standing up environment$NC"
-aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$bucketName.s3.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$bucketName ParameterKey=RemoteIp,ParameterValue=$trustedIp ParameterKey=RepositoryName,ParameterValue=$repoName ParameterKey=RepositoryUrl,ParameterValue=$repoUrl --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $region
+aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$bucketName.s3.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$bucketName ParameterKey=RepositoryName,ParameterValue=$repoName ParameterKey=RepositoryUrl,ParameterValue=$repoUrl --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $region
 else
 aws s3api create-bucket --bucket $bucketName --region $region --create-bucket-configuration LocationConstraint=$region
 aws s3 cp templates/ s3://$bucketName --recursive
 echo -e "$LB\n"
 echo -e "Standing up environment$NC"
-aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$bucketName.s3-$region.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$bucketName ParameterKey=RemoteIp,ParameterValue=$trustedIp ParameterKey=RepositoryName,ParameterValue=$repoName ParameterKey=RepositoryUrl,ParameterValue=$repoUrl --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $region
+aws cloudformation create-stack --stack-name falcon-ci-lab-stack --template-url https://$bucketName.s3-$region.amazonaws.com/entry.yaml --parameters ParameterKey=S3Bucket,ParameterValue=$bucketName ParameterKey=RepositoryName,ParameterValue=$repoName ParameterKey=RepositoryUrl,ParameterValue=$repoUrl --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $region
 fi
 echo -e "The Cloudformation stack will take 5-10 minutes to complete$NC"
 echo -e "\n\nCheck the status at any time with the command \n\naws cloudformation describe-stacks --stack-name falcon-ci-lab-stack --region $region$NC\n\n"
