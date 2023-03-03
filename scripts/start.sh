41cf17394aff26fb4a27f2d9ba116bdcee933be2
Ryan Payne <ryanj.payne@outlook.com>
2023-03-03 13:57:49 -0600

    remove cloudshare components

--- scripts/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-UNIQUE=$(cat /tmp/environment.txt | cut -c -8 | tr _ - | tr '[:upper:]' '[:lower:]')
-cd /home/ec2-user
-/home/ec2-user/falcon-ci-lab/scripts/deploy.sh up --unique_id="$UNIQUE" --trusted="$(curl -s http://ipinfo.io/ip)/32"
